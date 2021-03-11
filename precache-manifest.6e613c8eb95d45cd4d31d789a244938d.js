cf264e3ea6982e8c940335ed438025d37059f157
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-11 11:11:32 +0200

    Updates

---
+++ precache-manifest.6e613c8eb95d45cd4d31d789a244938d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42138f3e443676a0beddc71a71fa551a",
+    "url": "/Dawoudy/index.html"
+  },
+  {
+    "revision": "3f5e1b80c39d25c6aeed",
+    "url": "/Dawoudy/static/css/main.a48071f2.chunk.css"
+  },
+  {
+    "revision": "5b3b302eacec01ee4d45",
+    "url": "/Dawoudy/static/js/2.35fe996f.chunk.js"
+  },
+  {
+    "revision": "3f5e1b80c39d25c6aeed",
+    "url": "/Dawoudy/static/js/main.3aa992da.chunk.js"
+  },
+  {
+    "revision": "20e31e232e433004e5f6",
+    "url": "/Dawoudy/static/js/runtime-main.b2006d39.js"
+  },
+  {
+    "revision": "d63c16e6689a07bb2a8a41beb4c299f0",
+    "url": "/Dawoudy/static/media/homeHeader.d63c16e6.png"
+  },
+  {
+    "revision": "d49da23f2d4a108bc5fccee66aef00da",
+    "url": "/Dawoudy/static/media/homeHeader2.d49da23f.png"
+  },
+  {
+    "revision": "f70babb46b37b176e3d1f6bbb522eca7",
+    "url": "/Dawoudy/static/media/homeHeader3.f70babb4.png"
+  },
+  {
+    "revision": "798c24ea3352b267dd16988f815d505b",
+    "url": "/Dawoudy/static/media/logo.798c24ea.png"
+  }
+]);
